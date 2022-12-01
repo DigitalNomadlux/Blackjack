22dbb9ca66c06981dca3b1dcb935ae6e4112a1b7
Austin Turturro <82427116+DigitalNomadlux@users.noreply.github.com>
2022-11-30 23:35:12 -0600

    button functions

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
-let firstCard = 10;
+let firstCard = 6;
 let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
 let message = ""
+let cardsEl = document.getElementById("cards-el")
+let sumEl = document.getElementById("sum-el");
+let messageEl = document.getElementById("message-el");
 
-if (sum < 21) {
-  message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-  message = "Blackjack! You Win!";
-  hasBlackjack = true;
-} else {
-  message = "Try Again";
-  isAlive = false;
+function startGame() {
+  cardsEl.textContent = `Cards: ${firstCard}, ${secondCard}`;
+  sumEl.textContent = `Sum: ${sum}`;
+  if (sum < 21) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "Blackjack!";
+    hasBlackjack = true;
+  } else {
+    message = "Try Again";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+  
 }
 
-console.log(message);
+
+function drawCard() {
+  console.log("Drawing a new card for player")
+  let card = 4;
+  sum += card;
+  startGame();
+}
