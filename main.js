dd18e4da1ab675e5316486f0e60ff1f350b8059a
Austin Turturro <82427116+DigitalNomadlux@users.noreply.github.com>
2022-11-30 23:15:21 -0600

    basic js structure

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 let firstCard = 10;
 let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackjack = false;
+let isAlive = true;
+let message = ""
 
-let sum = firstCard + secondCard
-console.log(sum);
+if (sum < 21) {
+  message = "Do you want to draw a new card?";
+} else if (sum === 21) {
+  message = "Blackjack! You Win!";
+  hasBlackjack = true;
+} else {
+  message = "Try Again";
+  isAlive = false;
+}
+
+console.log(message);
