011953e7b55194b5e8a727e18cc93c0b3df600c0
Austin Turturro <82427116+DigitalNomadlux@users.noreply.github.com>
2022-12-04 22:19:45 -0600

    start of creating dynamic card draws

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let firstCard = 4;
-let secondCard = 10;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
@@ -13,8 +13,15 @@ function startGame() {
   renderGame();
 }
 
+function getRandomCard() {
+
+}
+
 function renderGame() {
-  cardsEl.textContent = `Cards: ${cards[0]}, ${cards[1]}`;
+  cardsEl.textContent = "Cards: "
+  for (i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " - ";
+  }
   sumEl.textContent = `Sum: ${sum}`;
   if (sum < 21) {
     message = "Do you want to draw a new card?";
@@ -31,7 +38,7 @@ function renderGame() {
 
 
 function drawCard() {
-  let card = 7;
+  let card = getRandomCard();
   sum += card;
   cards.push(card);
   renderGame();
